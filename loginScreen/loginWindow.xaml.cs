7c68d1cd499d9f03ff15b43909408b57e0a16ec1
Bikash Pokhrel <bpokhrel@aut.ac.nz>
2019-02-07 10:51:08 +1300

    Main window to login window

--- loginScreen/loginWindow.xaml.cs
+++ loginScreen/loginWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +24,48 @@ namespace loginScreen
         public loginWindow()
         {
             InitializeComponent();
+        }
+
+       private void BtnSubmit_OnClick(object sender, RoutedEventArgs e)
+        {
+            //1Create an object for Sql connection with sqlCon name
+            //1pass connection string inside that copy from SQL Server Object Explorer
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=BIKASH-PC\SQLEXPRESS;Initial Catalog=loginDB;Integrated Security=True");
+            //2 add try catch block
+            try
+            {
+                if (sqlCon.State == ConnectionState.Closed)
+                    sqlCon.Open();//2sql function
+                //4 declare query 
+                String query = "SELECT COUNT(1) FROM tblUser WHERE Username = @Username AND Password =@Password";
+                //5 Create an object of SqlCommand with first parameter as query and second sql connection object sqlCon 
+                SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                sqlCmd.CommandType = CommandType.Text;
+                //6 passing values for username and password
+                sqlCmd.Parameters.AddWithValue("@Username", txtUsername.Text);
+                sqlCmd.Parameters.AddWithValue("@Password", txtPassword.Password);
+                //declare variable count and executer this command
+                int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                if (count == 1)
+                {
+                    MainWindow dashboard = new MainWindow();
+                    dashboard.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Username or Password is Incorrect");
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);//2exception message passed
+            }
+            finally
+            {
+                sqlCon.Close();//3 close sql connection
+            }
+
         }
     }
 }
